b3ae7c3700f14736215f18e0adeefdb5c1630162
Prashant pandey <prashantpandey9812@gmail.com>
2025-10-11 05:05:08 +0000

    datatype

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -6,3 +6,24 @@ console.log(3+3);
 // code redability should be high 
 console.log("hello bhai");
 
+let name = "prashant "
+let age = 18
+let isLoggedIn = false 
+let state;
+let city = null;
+
+// data types in java script ....
+// number =>2 to power 53
+// bigint this will use in trading or big website
+//string => ""
+// boolean => true/false.
+//null =>stand alone value 
+// undefined => mean value is not defined yet .
+// symbol => unique 
+
+
+// object 
+console.log(typeof age);
+
+console.log(typeof city); //type is object 
+console.log(typeof state);// type is undefined 
