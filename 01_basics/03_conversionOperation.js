3f5f8c2cf056a542f0467fdedfe71d143f23f3ed
Prashant pandey <prashantpandey9812@gmail.com>
2025-10-11 05:45:30 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+// let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+//if value of score is :- after convert in number 
+// "33"=> ye easy convert ho jata h 33 m 
+// "33abc" => NaN-> NotaNumber
+//null => 0 is the value 
+// undefined => NaN
+// true/false => 1/0
+// "String value " =>Nan
+
+
+
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=> true; 0>= false 
+// "" => false 
+// "prashant" =>true
+// 33 => true
+
+let someNumber = 45
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
