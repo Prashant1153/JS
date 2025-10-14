f597f66ae809ea0ec106998aba07e5c6542dff6c
Prashant pandey <prashantpandey9812@gmail.com>
2025-10-14 15:00:42 +0000

    Prefix & Postfix

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -57,7 +57,36 @@
 // let str3 = str1+str2
 // console.log(str3);
 
-console.log("1"+2);
-console.log(1+"2");
-console.log(1+"2"+2);
-console.log(1+2+"2");
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log(1+"2"+2);
+// console.log(1+2+"2");
+// console.log((3+4)*5%3);
+
+// console.log(+true);// result mai 1 ayega.
+// console.log(true+); // error ayega.
+// console.log(+""); // result m 0 ayega .
+
+
+
+// POSTFIX : 
+let x = 3;
+const y = x++; // iske andar phele y ki value assigend hui uske baad increment hui h x ki value ..
+
+console.log(`x:${x}, y:${y}`); 
+// redabale format ke liye h ye aur better format ke liye ${x} and ${y}: These get replaced with the values of variables x and y when the code runs.
+// Expected output: "x:4, y:3"
+//PREFIX:
+let a = 3;
+const b = ++a;// iske andar phele increment hua h uske badd hua b ki value assigned ..
+
+console.log(`a:${a}, b:${b}`);
+ // Backticks (`) start and end a template literal.
+
+//You can use ${...} to embed variables or even calculations.
+
+//Perfect for multi-line or dynamic strings.
+// Expected output: "a:4, b:4"
+
+
+
