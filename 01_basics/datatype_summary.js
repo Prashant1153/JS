0e303e0cfb3f119582defe9dd0d9bf15e993859c
Prashant pandey <prashantpandey9812@gmail.com>
2025-10-14 17:47:51 +0000

    datatype sumary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,38 @@
+//Primitive:
+ 
+// 7 types : String ,Number ,null,Boolean,,Undefined,Symbol,BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null// type of operator object ata h 
+let userEmail;
+
+const id = Symbol('123')// return type milta h woo bhi alag data type hi milta h  
+const anotherId = Symbol('123')
+
+let x = 10;      // x is a number
+x = "hello";     // now x is a string
+x = [1, 2, 3];   // now x is an array
+
+console.log(id=== anotherId)
+
+const bigNumber = 28292550016516565066065656166161619161611611601 // undefined 
+
+
+// Reference (Non primitive)
+
+// Array,Objects,Functions
+ const heros = ["shaktiman","nagraj","superman"]
+ let myObject = {
+    name: "prashant",
+    age: 20,
+ }
+
+const myFunstion = function(){
+    console.log("hello world");
+     // type of datatype object function 
+}
+
+console.log(typeof bigNumber);
