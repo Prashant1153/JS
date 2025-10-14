5d4ae6688592106c44f740f5311316e06246862d
Prashant pandey <prashantpandey9812@gmail.com>
2025-10-14 17:10:43 +0000

    if else

--- 01_basics/04_comaprision.js
+++ 01_basics/04_comaprision.js
@@ -4,3 +4,21 @@
 // console.log(2==1);
 // console.log(2!=1);
 
+
+// console.log("2">1); comparison ke andar ye string ko value man lete h 
+// console.log("02">1);
+
+console.log(null>0);//false 
+console.log(null==0);//false 
+console.log(null>=0);//true  because >= or<= null jo number m convert ho jata h , treatng it as 0.
+
+// NOTE : iss tarike ke comparision avoid karna chaiye aur confuse kar dete h clean code karo 
+console.log(undefined ==0);
+console.log(undefined >0);
+console.log(undefined <0);// false in every condition 
+
+// === ye stricly check karta h value aur data type dono ko 
+console.log("2"===2);
+
+
+
