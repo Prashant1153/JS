ec5953fa133b86e56773f444bddb72b63867cb4f
Prashant pandey <prashantpandey9812@gmail.com>
2025-10-30 06:20:49 +0000

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,4 +1,12 @@
 // array
 // new only for streak
 
-const myarray = [0,1,2,3,4,5]
+const myarray = [0,1,2,3,4,5];
+const newrray = [0,1,2,3,4,5];
+console.log(myarray == newrray); 
+console.log(JSON.stringify(myarray) === JSON.stringify(newrray));// ye true dega kyuki 
+
+myarray.push(67);
+const lastvalue = myarray.pop(8);
+console.log(myarray);
+console.log(lastvalue);
